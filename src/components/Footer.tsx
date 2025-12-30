8a47db28d3833fba443e2547bb9215d5d5e8b31f
AnkithaKulal493 <ankitha@connectia.in>
2025-12-30 17:29:46 +0530

    third commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,13 @@
+import { Linkedin,Github } from 'lucide-react';
+
 const Footer = () => {
   return (
-    <footer className="text-center py-6 bg-color1 border-t border-white/10">
-        <p className="text-gray-300">Ankitha | Web Developer</p>
+    <footer className="justify-center flex gap-20 py-6 bg-color1 border-t border-white/10">
+        <p className="text-gray-300 text-center">Ankitha | Web Developer</p>
+        <div className="flex gap-5 mt-1">
+          <a href="https://www.linkedin.com/in/ankitha-kulal-b92215281" target='_blank'><Linkedin size={20}/></a>
+          <a href="https://github.com/AnkithaKulal27" target='_blank'><Github size={20}/></a>
+        </div>
     </footer>
   )
 }
